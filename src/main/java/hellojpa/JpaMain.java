3445bb21262a279bd128e2bdd54bcb5da47d8437
Yeriimii <seojs0511@naver.com>
2023-08-19 14:15:13 +0900

    영속성 컨텍스트2 - Dirtry checking

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,11 +18,10 @@ public class JpaMain {
         try {
 
             //영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ"); // Java collection 다루듯 변경 가능 <- Dirty checking
 
-            em.persist(member1); // 쓰기 지연
-            em.persist(member2); // 쓰기 지연
+//            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
