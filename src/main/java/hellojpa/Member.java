31fb17ab2204f8da5c1204e0527f7dff135a991c
Yeriimii <seojs0511@naver.com>
2023-08-21 01:26:29 +0900

    일대일 관계 * 단방향은 지원 X * 양방향만 지원

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -14,6 +14,10 @@ public class Member {
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false) // 일대다 양방향: 읽기 전용으로 만듦
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
     public Long getId() {
         return id;
     }
